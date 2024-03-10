750fc5edc6d6952a691323dbcb57f46841d7d4c4
luisaraque1988 <luisaraque1988@gmail.com>
2024-03-10 19:14:25 -0300

    Add UI
    I add user interface to the game, and a file to add some style.
    I could get the buttons to work, but I cannot stop the game when either
    the computer or the player win, also, I cannot reset the game

--- myScript.js
+++ myScript.js
@@ -11,7 +11,7 @@ function getComputerChoice() {
     return answer;
 }
 
-function singleRound(playerSelection, computerSelection) {
+function playOneRound(playerSelection, computerSelection) {
     
     let user = playerSelection.toLowerCase();
     let result;
@@ -21,15 +21,15 @@ function singleRound(playerSelection, computerSelection) {
     }
 
     if (user === 'rock' && computerSelection === '3') {
-        result = 'You Win! Rock beats scissors';
+        result = 'You win! Rock beats scissors';
     } else if (user === 'scissors' && computerSelection === 'rock'){
         result = 'Computer wins! Rock beats scissors'
     } else if (user === 'paper' && computerSelection === 'rock') {
-        result = 'You Win! paper beats rock'
+        result = 'You win! paper beats rock'
     } else if (user === 'rock' && computerSelection === 'paper'){
         result = 'Computer wins! paper beats rock'
     } else if (user === 'scissors' && computerSelection === 'paper') {
-        result = 'You Win! scissors beats paper'
+        result = 'You win! scissors beats paper'
     } else if (user === 'paper' && computerSelection === 'scissors'){
         result = 'Computer wins! scissors beats paper'
     }
@@ -37,7 +37,7 @@ function singleRound(playerSelection, computerSelection) {
     return result;
 }
 
-function playGame() {
+/*function playGame() {
     
     let userCount = 0;
     let computerCount = 0;
@@ -45,12 +45,12 @@ function playGame() {
     for(let i = 0; i < 5; i++) {
         let computerSelection = getComputerChoice();
         let playerSelection = prompt('Introduce your option');
-        let resultSingleRound = singleRound(playerSelection,computerSelection);
-        console.log(resultSingleRound);
+        let resultOneRound = playOneRound(playerSelection,computerSelection);
+        console.log(resultOneRound);
 
-        if (resultSingleRound.slice(0,8) === 'You Win!') {
+        if (resultOneRound.slice(0,8) === 'You Win!') {
             userCount += 1;
-        } else if (resultSingleRound.slice(0,14) === 'Computer wins!') {
+        } else if (resultOneRound.slice(0,14) === 'Computer wins!') {
             computerCount += 1;
         }
     }
@@ -62,4 +62,53 @@ function playGame() {
     }
 }
 
-console.log(playGame());
+console.log(playGame());*/
+
+const buttons = document.querySelectorAll('button');
+const body = document.querySelector('body');
+
+const results = document.createElement("ul");
+
+const resultPlayer = document.createElement("li");
+resultPlayer.textContent = "Player count = 0";
+
+const resultComputer = document.createElement("li");
+resultComputer.textContent = "Computer count = 0";
+
+const totalScore = document.createElement("li");
+totalScore.textContent = "Result = "
+
+results.appendChild(resultComputer);
+results.appendChild(resultPlayer);
+results.appendChild(totalScore);
+body.appendChild(results);
+
+let computerCount = 0;
+let playerCount = 0;
+
+buttons.forEach(function (i) {
+    i.addEventListener("click", function() {
+
+        let playerSelection = this.innerHTML;
+        let computerSelection = getComputerChoice();
+        oneRound = playOneRound(playerSelection, computerSelection);
+        alert(oneRound)
+
+        
+        if (oneRound.slice(0, 8) === "You win!") {
+            playerCount += 1;
+            resultPlayer.textContent = `Player count = ${playerCount}`;
+        } else if (oneRound.slice(0,14) === 'Computer wins!') {
+            computerCount += 1;
+            resultComputer.textContent = `computer count = ${computerCount}`;
+        }
+        
+        
+        if (computerCount == 5) {
+            totalScore.textContent = "Result = Computer wins, it beats you 5 times";
+        } else if (playerCount == 5) {
+            totalScore.textContent = "Result = You win, you beat the computer 5 times"
+        }
+    });
+});
+
